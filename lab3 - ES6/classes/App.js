d2cbad4fb3af70a87beab074e40a5b5b99fe132d
Bram Colleman <bram@laptop-dtamjb5r.guests.thomasmore.be>
2022-03-03 12:44:23 +0100

    feat: add features such as add, remove and mark notes as done

--- lab3 - ES6/classes/App.js
+++ lab3 - ES6/classes/App.js
@@ -3,35 +3,21 @@ import Todo from './Todo.js';
 
 export default class App {
     constructor() {
-      this.setupEventListeners();      // set up the enter Key
-      this.loadFromStorage();      // when the app loads, we can show previously saved items from localstorage
+      this.setupEventListeners(); 
+      this.loadFromStorage();
     }
   
     setupEventListeners() {
-      // pressing the enter key in the text field triggers the createItem function
-      // addEventListener("keyup", this.createItem.bind(this));
       document.querySelector("#add-item-text").addEventListener("keyup", this.createItem.bind(this));
-      // read up on .bind() -> we need to pass the current meaning of this to the eventListener
-      // while testing, feel free to console.log(this) to see what's in it
     }
   
     createItem(e) {
-        if (e.key === 'Enter'){
+        if (e.key === 'Enter' && document.querySelector("#add-item-text").value != ""){
             let todo = new Todo(document.querySelector("#add-item-text").value);
-            console.log(todo.title);
+            todo.add();
+            todo.saveToStorage();
+            this.reset();
         }
-        // this.reset();
-        // console.log(this);
-
-
-      // HINT🤩
-      // this function should create a new todo by using the Todo() class
-      // new Todo(text)
-      // todo.add();
-      // todo.saveToStorage();
-      // if you used bind() in the previous function, you'll notice that this refers to the current class instance
-      // clear the text field with .reset() after adding the item
-      // if (e.key === "Enter")
     }
   
     loadFromStorage() {
@@ -41,7 +27,6 @@ export default class App {
     }
   
     reset() {
-      // this function should reset the form / clear the text field
-
+        document.querySelector("#add-item-text").value = "";
     }
   }
